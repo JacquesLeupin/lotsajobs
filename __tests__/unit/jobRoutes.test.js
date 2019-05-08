b26d6dc7758d658259cae98c8722eef5b72d3822
odoland <orlando.wong@yahoo.com>
2019-05-08 16:10:38 -0700

    fixed truncate table for companyRoutes test

--- __tests__/unit/jobRoutes.test.js
+++ __tests__/unit/jobRoutes.test.js
@@ -49,8 +49,7 @@ beforeEach(function () {
       UCSF.description,
       UCSF.logo_url
     ])
-}
-)
+})
 
 
 afterEach(function () {
@@ -60,40 +59,40 @@ afterEach(function () {
 
 
 // CRUD - Read route 
-describe("GET /companies", function () {
+// describe("GET /companies", function () {
 
 
-  test("Gets a list of all companies", async function () {
-    const response = await request(app).get(`/companies`);
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ "companies": [{ "description": "Where Jax used to work", "handle": "AROUND", "logo_url": "lolgetoutofhere", "name": "getAround", "num_employees": 2000 }] })
-  })
-  // Test the gets with query parameters
+//   test("Gets a list of all companies", async function () {
+//     const response = await request(app).get(`/companies`);
+//     expect(response.statusCode).toBe(200);
+//     expect(response.body).toEqual({ "companies": [{ "description": "Where Jax used to work", "handle": "AROUND", "logo_url": "lolgetoutofhere", "name": "getAround", "num_employees": 2000 }] })
+//   })
+//   // Test the gets with query parameters
 
-  test("Gets a list of all companies", async function () {
-    const response = await request(app).get(`/companies?search=getAround`);
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ "company": [{ "handle": "AROUND", "name": "getAround" }] })
-  })
+//   test("Gets a list of all companies", async function () {
+//     const response = await request(app).get(`/companies?search=getAround`);
+//     expect(response.statusCode).toBe(200);
+//     expect(response.body).toEqual({ "company": [{ "handle": "AROUND", "name": "getAround" }] })
+//   })
 
-  test("Return 404 for invalid query params", async function () {
-    const response = await request(app).get(`/companies?search=getAround&min_employees=3000&max_employees=200`);
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual({ "message": "Please give a valid range", "status": 400 })
-  })
+//   test("Return 404 for invalid query params", async function () {
+//     const response = await request(app).get(`/companies?search=getAround&min_employees=3000&max_employees=200`);
+//     expect(response.statusCode).toBe(400);
+//     expect(response.body).toEqual({ "message": "Please give a valid range", "status": 400 })
+//   })
 
-  test("Return specific company based on valid query params", async function () {
-    const response = await request(app).get(`/companies?min_employees=300&max_employees=2001`);
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ "company": [{ "handle": "AROUND", "name": "getAround" }] })
-  })
+//   test("Return specific company based on valid query params", async function () {
+//     const response = await request(app).get(`/companies?min_employees=300&max_employees=2001`);
+//     expect(response.statusCode).toBe(200);
+//     expect(response.body).toEqual({ "company": [{ "handle": "AROUND", "name": "getAround" }] })
+//   })
 
-  test("Return nothing if no companies satisfy valid query params", async function () {
-    const response = await request(app).get(`/companies?min_employees=300&max_employees=2000`);
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ "company": [] })
-  })
-})
+//   test("Return nothing if no companies satisfy valid query params", async function () {
+//     const response = await request(app).get(`/companies?min_employees=300&max_employees=2000`);
+//     expect(response.statusCode).toBe(200);
+//     expect(response.body).toEqual({ "company": [] })
+//   })
+// })
 
 // create a new job
 describe("POST /jobs", function () {
@@ -109,68 +108,21 @@ describe("POST /jobs", function () {
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual();
 
-  test("Route successfully posts the job posting", async function () {
-    const response = await request(app).post(`/jobs`)
-      .send({
+    test("Route successfully posts the job posting", async function () {
+      const response = await request(app).post(`/jobs`)
+        .send({
+          title: "Singer",
+          salary: 12345,
+          equity: 0.5,
+          company_handle: "UCSF"
+        });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual({
         title: "Singer",
         salary: 12345,
         equity: 0.5,
         company_handle: "UCSF"
-      });
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({
-      title: "Singer",
-      salary: 12345,
-      equity: 0.5,
-      company_handle: "UCSF"
+      })
     })
   })
-
-
-
-
-// // UPDATE 
-// describe("PATCH /companies/:handle", function () {
-//   test("Updates a single company", async function () {
-//     const response = await request(app)
-//       .patch(`/companies/AROUND`)
-//       .send({
-//         "handle": "GGLY",
-//         "name": "Gooooo===ogly",
-//         "num_employees": 1234,
-//         "description": "Where Jax will work",
-//         "logo_url": "www.lolgetoutofhere.com"
-//       })
-
-//     expect(response.statusCode).toBe(200);
-
-//     expect(response.body).toEqual({
-//       "company": {
-//         "description": "Where Jax will work",
-//         "handle": "GGLY",
-//         "logo_url": "www.lolgetoutofhere.com",
-//         "name": "Gooooo===ogly",
-//         "num_employees": 1234,
-//       },
-//     })
-//   })
-
-//   // invalid handle
-//   test("Responds with 404 if id invalid", async function () {
-//     const response = await request(app).patch(`/companies/asdlkfjlasdjf`)
-
-//     expect(response.statusCode).toBe(404)
-//     expect(response.body.message).toEqual("Company not found!")
-//   });
-// });
-
-
-// // DELETE 
-// describe("DELETE /companies/:handle", function () {
-//   test("Deletes a single a company", async function () {
-//     const response = await request(app)
-//       .delete(`/companies/AROUND`)
-//     expect(response.statusCode).toBe(200)
-//     expect(response.body).toEqual({ message: "Company deleted" })
-//   });
-// });
+})
