726d7d8bbfde44c28098d05b6ce2e1c5401a6b42
JacquesLeupin <jacquesleupin@gmail.com>
2019-05-08 17:39:37 -0700

    modifying teardown of test

--- __tests__/unit/jobRoutes.test.js
+++ __tests__/unit/jobRoutes.test.js
@@ -62,29 +62,7 @@ afterEach(async function () {
 })
 
 afterAll(async function () {
-
-  await db.query(`
-  DROP TABLE IF EXISTS companies, jobs;
-  
-  CREATE TABLE companies (
-    handle TEXT PRIMARY KEY,
-    name TEXT,
-    num_employees INTEGER,
-    description TEXT, 
-    logo_url TEXT
-  );
-  
-  CREATE TABLE jobs (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    salary FLOAT NOT NULL,
-    equity FLOAT CHECK (equity > 0 AND equity <1) NOT NULL, 
-    company_handle TEXT REFERENCES companies ON DELETE CASCADE,
-    date_posted TIMESTAMP WITHOUT TIME ZONE NOT NULL
-  );
-  
-  `)
-
+    await db.end()
 })
 
 
